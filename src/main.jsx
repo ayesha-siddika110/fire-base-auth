a17b6a93f41936e316de783650bd11343e1ece56
Ayesha Siddika <ayeshasiddika777697@gmail.com>
2024-11-13 23:19:38 +0600

    componants added and resigster code import

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './componants/Root/Root.jsx';
+import Register from './componants/Register/Register.jsx';
+import Login from './componants/Login/Login.jsx';
+import Home from './componants/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
